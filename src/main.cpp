23c0cb5e94d55cb83ba811c8619f22f5d498abf7
maxfaber <maxfaber@quicknet.nl>
2023-02-10 16:32:45 +0100

    Removed libtorch dependency from project

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <main.h>
-#include <torch/torch.h>
 
 int main(int argc, char** argv)
 {
-//    torch::TensorOptions tensorOptions = torch::TensorOptions().device(torch::kMPS);
-    torch::TensorOptions tensorOptions = torch::TensorOptions().device(torch::kCPU);
-    torch::Tensor x = torch::randn({2, 2}, tensorOptions);
-
-    std::cout << x << "\n";
     Graphics::init(argc, argv, GameRendering::initialWidth, GameRendering::initialHeight);
     GameState::initialize();
     GameRendering::show();
